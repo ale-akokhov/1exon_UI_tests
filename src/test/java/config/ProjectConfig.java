5f7f0ececf2354a5e10642df690cffe2d3c1b004
Alexander <ale-akokhov@ayndex.ru>
2026-09-11 20:58:37 +0300

    Project created

---
+++ src/test/java/config/ProjectConfig.java
@@ -0,0 +1,30 @@
+package config;
+
+import org.aeonbits.owner.Config;
+
+@Config.Sources({
+        "system:properties",
+        "classpath:config/system.properties"
+})
+
+public interface ProjectConfig extends Config {
+
+    @Key("baseUrl")
+    @DefaultValue("https://1exon.ru/")
+    String getBaseUrl();
+
+    @Key("remoteUrl")
+    String getRemoteUrl();
+
+    @Key("browser")
+    @DefaultValue("chrome")
+    String getBrowser();
+
+    @Key("browserSize")
+    @DefaultValue("1920x1080")
+    String getBrowserSize();
+
+    @Key("browserVersion")
+    @DefaultValue("152.0")
+    String getBrowserVersion();
+}
